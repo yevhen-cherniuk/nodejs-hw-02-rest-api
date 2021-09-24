5a30394d9e15f00fce1421f12f5ed0d679600dff
yevhen-cherniuk <chernuy1@gmail.com>
2021-09-24 05:12:24 +0400

    refactoring code

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,20 @@
+const mongoose = require('mongoose')
 const app = require('../app');
-const db = require('../model/db');
-
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const { DB_HOST, PORT = 3000 } = process.env
 
-db.then(() => {
+mongoose
+  .connect(DB_HOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+  console.log('Database connection successful')
   app.listen(PORT, () => {
     console.log(`Server running. Use our API on port: ${PORT}`);
   });
 }).catch(error => {
-  console.log(`Error: ${error.message}`);
+  console.log(`Error: ${error.message}`)
+  process.exit(1)
 });
