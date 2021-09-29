975d7397735803eecdcbb6e790ffb12772be2606
yevhen-cherniuk <chernuy1@gmail.com>
2021-09-30 03:11:19 +0400

    add sendgrid

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,4 +1,6 @@
 const Users = require('../../repositories/users');
+const EmailService = require('../../services/email');
+const { CreateSenderSendGrid } = require('../../services/email-sender');
 
 const register = async (req, res, next) => {
   try {
@@ -11,11 +13,22 @@ const register = async (req, res, next) => {
         message: 'Email in use',
       });
     }
-    const { email, subscription } = await Users.create(req.body);
+    const { email, subscription, avatarURL, verifyToken } = await Users.create(
+      req.body,
+    );
+    try {
+      const emailService = new EmailService(
+        process.env.NODE_ENV,
+        new CreateSenderSendGrid(),
+      );
+      await emailService.sendVerifyEmail(verifyToken, email);
+    } catch (error) {
+      console.log(error.message);
+    }
     return res.status(201).json({
       status: 'success',
       code: 201,
-      user: { email, subscription },
+      user: { email, subscription, avatarURL, verifyToken },
     });
   } catch (error) {
     next(error);
